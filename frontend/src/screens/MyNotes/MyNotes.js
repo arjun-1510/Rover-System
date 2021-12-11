3d1711b4c0e19e1ff54f9c2dfae0c92c3770c6bf
arjun-1510 <Arjung1510@gmail.com>
2021-12-11 09:06:32 +0530

    accordian

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Badge, Button, Card } from 'react-bootstrap';
+import {  Badge, Button, Card } from 'react-bootstrap';
 import MainScreen from '../../components/MainScreen';
 import {Link}  from "react-router-dom"
 import { fontSize } from '@mui/system';
 import notes,{} from "../../data/notes";
+import Accordion from 'react-bootstrap/Accordion'
+
+
+
 
 
 const MyNotes = () => {
@@ -26,46 +30,56 @@ const MyNotes = () => {
             </Link>
              {
                  notes.map(note => (
-                    <Card style={{margin:10}}>
-                    <Card.Header style={{display:"flex"}}>
-                        <div  style={{
-                            color:"block",
+
+                    <Accordion>
+                    <Card style={{ margin: 10 }} key={note._id}>
+                      <Card.Header style={{ display: "flex" }}>
+                        <span
+                          // onClick={() => ModelShow(note)}
+                          style={{
+                            color: "black",
                             textDecoration: "none",
                             flex: 1,
                             cursor: "pointer",
                             alignSelf: "center",
                             fontSize: 18,
-    
-                        }}>
+                          }}
+                        >
+                          <Accordion.Toggle
+                            as={Card.Text}
+                            variant="link"
+                            eventKey="0"
+                          >
                             {note.title}
+                          </Accordion.Toggle>
+                        </span>
+      
+                        <div>
+                          <Button href={`/note/${note._id}`}>Edit</Button>
+                          <Button
+                            variant="danger"
+                            className="mx-2"
+                            onClick={() => deleteHandler(note._id)}
+                          >
+                            Delete
+                          </Button>
                         </div>
-                   
-                    <div>
-                        <Button href={`/notes/${note._id}`}>Edit</Button>
-                        <Button variant='danger' className="mx-2" onClick={() => deleteHandler(note._id)}>Delete</Button>
-                    </div>
-                    </Card.Header>
-                    <Card.Body>
-                        <h4>
-                            <Badge bg='info' >
-                                Category - {note.category}
+                      </Card.Header>
+                      <Accordion.Collapse eventKey="0">
+                        <Card.Body>
+                          <h4>
+                            <Badge variant="success">
+                              Category - {note.category}
                             </Badge>
-                        </h4>
-
-
-
-                  <blockquote className="blockquote mb-0">
-                   <p>
-                      
-                   {note.content}
-                  </p>
-                   <footer className="blockquote-footer">
-                    Creted On-Date
-                   </footer>
-                 </blockquote>
-                   </Card.Body>
-                </Card>
-
+                          </h4>
+                          <blockquote className="blockquote mb-0">
+                            <div>{note.content}</div>
+                            
+                          </blockquote>
+                        </Card.Body>
+                      </Accordion.Collapse>
+                    </Card>
+                  </Accordion>
                  ))
              }
 
